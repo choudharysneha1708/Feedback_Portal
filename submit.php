fccb4ffc0124d36a47d027071bc1330d8878d29e
Sneha Choudhary <97403777+choudharysneha1708@users.noreply.github.com>
2023-06-21 12:53:43 +0530

    Add files via upload

--- submit.php
+++ submit.php
@@ -2,26 +2,16 @@
 require('../../class/Student.php');
 $student = new Student();
 session_start();
-$name = $_POST['name'];
 $password = $_POST['password'];
-$username = $_SESSION['username'];
-$batch = $_SESSION['batch'];
-$department = strtoupper($_SESSION['department']);
-$program = $_SESSION['program'];
 $email = $_SESSION['email'];
+$username = substr($email, 0, 10);
 $hash = password_hash($password, PASSWORD_BCRYPT);
-$pseudo_name = md5($username);
-$role = "student";
-if(isset($_SESSION['username'])){
-  $submit = "INSERT INTO P2_Student (`enroll_no`, `name`, `dept_name`, `batch`, `program`) VALUES ('$username', '$name', '$department', '$batch', '$program')";
-  $result = $student->run_query($submit);
-  $login = "INSERT INTO P2_Login (`login_id`, `hash`, `role`) VALUES ('$username', '$hash', '$role')";
+if(isset($_SESSION['email'])){
+  $login = "UPDATE P2_Login SET `hash` = '$hash' WHERE login_id = '$email' OR login_id =  '$username'";
   $result = $student->run_query($login);
-  $mapping = "INSERT INTO P2_Mapping (`enroll_no`, `pseudo_name`) VALUES ('$username','$pseudo_name')";
-  $result = $student->run_query($mapping);
   $delete = "DELETE FROM P2_Verification where email = '$email'";
   $result = $student->run_query($delete);
 session_destroy();
-echo "Registration Complete";
+echo "Password Updated";
 }
 ?>
